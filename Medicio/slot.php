bd3625073cca8c706cfa9c4549c5509fa5d95505
BTejesh27 <btejesh_csd@srkrec.edu.in>
2024-02-16 08:31:09 +0530

    tejesh

--- Medicio/slot.php
+++ Medicio/slot.php
@@ -90,7 +90,7 @@ $conn->close();
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-  <title>BHIMAVARAM HOSPITALS</title>
+  <title>Hospital</title>
   <meta content="" name="description">
   <meta content="" name="keywords">
 
